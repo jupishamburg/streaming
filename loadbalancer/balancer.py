a71ede4c92b065b39dbfcc723ad013b993e4a722
Fritz Grimpen <fritz@grimpen.net>
2012-10-21 12:59:36 +0000

    Using argparse to make it usable

--- loadbalancer/balancer.py
+++ loadbalancer/balancer.py
@@ -1,57 +1,37 @@
 #!/usr/bin/env python
-from bottle import route, run, response, redirect
+import bottle
+from bottle import run, response, redirect
 from operator import itemgetter
 import urllib.request
 import json
 import time
 import threading
  
-config = {
-	"servers": [
-		{
-			"url": "gododdin.techel.net:8000",
-			"max-listeners": 100,
-			"current-listeners": 0,
-			"free-slots": 100
-		},
-		{
-			"url": "cexy.techel.net:8000",
-			"max-listeners": 100,
-			"current-listeners": 0,
-			"free-slots": 100
-		}
-	],
-	"update-interval": 15,
-
-	"webserver": {
-		"host": "0.0.0.0",
-		"port": 8080,
-		"frontend": "tornado"
-	}
-}
- 
 # stats refresher thread
 class FreeSlotThread(threading.Thread):
 	daemon = True
  
-	def __init__(self):
+	def __init__(self, config):
 		threading.Thread.__init__(self)
+		self.config = config
  
 	def run(self):
 		while True:
-			for server in config["servers"]:
+			for server in self.config["servers"]:
 				url = "http://{0}/json.xsl".format(server["url"])
 				stats = json.loads(urllib.request.urlopen(url).read().decode('utf-8'))
 				server["current-listeners"] = int(stats["total_listeners"])
 				server["free-slots"] = server["max-listeners"] - int(stats["total_listeners"])
  
-			time.sleep(config["update-interval"])
- 
+			time.sleep(self.config["update-interval"])
+
+app = bottle.Bottle()
+
 # playlist deliver
-@route("/<mount>.m3u")
+@app.route("/<mount>.m3u")
 def playlist(mount):
 	urls = []
-	servers = sorted(config["servers"], key=itemgetter("free-slots"), reverse=True)
+	servers = sorted(app.config["servers"], key=itemgetter("free-slots"), reverse=True)
 	for s in servers:
 		urls.append("http://{0}/{1}".format(
 			s["url"],
@@ -62,9 +42,9 @@ def playlist(mount):
 	return "\n".join(urls)
 
 # redirect deliver (for html5 audio players, …)
-@route("/<mount>")
+@app.route("/<mount>")
 def redirector(mount):
-	s = sorted(config["servers"], key=itemgetter("free-slots"), reverse=True)[0]
+	s = sorted(app.config["servers"], key=itemgetter("free-slots"), reverse=True)[0]
 	url = "http://{0}/{1}".format(
 		s["url"],
 		mount
@@ -73,10 +53,10 @@ def redirector(mount):
 	redirect(url, code=307)
  
 # stats deliver
-@route("/")
+@app.route("/")
 def stats():
 	out = '<table border="1"><tr><th>Listeners</th><th>Slots</th><th>Free slots</th><th>Server</th></tr>'
-	servers = sorted(config["servers"], key=itemgetter("free-slots"), reverse=True)
+	servers = sorted(app.config["servers"], key=itemgetter("free-slots"), reverse=True)
 	for s in servers:
 		out += '<tr><td>{0}</td><td>{1}</td><td>{2}</td><td><a href="http://{3}">{3}</a></td></tr>'.format(
 			s["current-listeners"],
@@ -87,10 +67,19 @@ def stats():
  
 	out += '</table>'
 	return out
+
+if __name__ == '__main__':
+	import argparse
+	argparser = argparse.ArgumentParser(description="Load balancer for Icecast2 streams")
+	argparser.add_argument('-c', '--config', type=open, default="balancer.json")
+	argparser.add_argument('-s', '--server', type=str, default="tornado")
+	argparser.add_argument('-H', '--host', type=str, default="0.0.0.0")
+	argparser.add_argument('-p', '--port', type=int, default=8080)
+	args = argparser.parse_args()
+
+	config = json.load(args.config)
+	FreeSlotThread(config).start()
+
+	app.config.update(config)
+	run(app, server=args.server, host=args.host, port=args.port)
  
-# run the thread
-stats = FreeSlotThread()
-stats.start()
- 
-# run the http server
-run(server=config["webserver"]["frontend"], host=config["webserver"]["host"], port=config["webserver"]["port"])
