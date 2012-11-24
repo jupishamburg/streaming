ab450152140a3ee13a0f87762ba5da8ce8242081
Fritz Grimpen <fritz@grimpen.net>
2012-11-24 12:23:21 +0000

    Removed old implementation

--- backend/stroem/__init__.py
+++
@@ -1,8 +0,0 @@
-import bottle
-from beaker.middleware import SessionMiddleware
-
-b_app = bottle.Bottle()
-app = SessionMiddleware(b_app, {
-	'session.type': 'memory',
-	'session.secret': 'oKuoc2eijeifahwe3uv5koo3Po4Een2thahL0phaech4iephai1Cheigh6Shohso'
-})
